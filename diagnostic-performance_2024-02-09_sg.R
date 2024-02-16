a915a333f676596be86b93927dbd150448f1ac52
Stefan Gehrig <stefan-gehrig@t-online.de>
2024-02-16 15:45:32 +0100

    changes in diagn_perf

--- diagnostic-performance_2024-02-09_sg.R
+++ diagnostic-performance_2024-02-09_sg.R
@@ -1,3 +1,9 @@
+# eidted by stefan gehrig, 2024-02-16
+# - taken out calculations not needed for FSP validation project
+# - renaming of some columns
+# - allow to specify a prevalence for NPV / PPV calculations and their intervals
+# - added calculations for OAPR
+
 ################################################################################
 ## Function diagn_perf                                                        ##
 ##                                                                            ##
@@ -12,6 +18,8 @@
 ##    +  test_outcome: name of binary column with predicted outcome classes   ##
 ##    +  test_pos:     value of pred_outcome to be considered positive -      ##
 ##                     all other values are considered negative)              ##
+##    +  prevalence:   prevalence at which NPV and PPV are compuated;         ##
+##                     cannot be left empty.                                  ##
 ##    +  conf_level:   level for 2-sided confidence intervals                 ##
 ##    +  conf_type:    CIs of Sensitivity, Specificity, FPR, PPV, NPV         ##
 ##                     according to "Wilson" or acc. to "Clopper-Pearson"     ##
@@ -21,6 +29,7 @@
 ##                     Sensitivity, Specificity, FPR, PPV, NPV                ##
 ##    +  n_dig_lr:     number of digits for pretty print output for           ##
 ##                     LR+, LR-                                               ##
+##    +  n_dig_odds:   number of digits for odds                              ##
 ##                                                                            ##
 ## Output:                                                                    ##
 ##    +  dataframe with one row                                               ##
@@ -37,9 +46,9 @@
 ##                                                                            ##
 ##                                                                            ##
 ################################################################################
-
 diagn_perf <- function( d, ref_outcome, ref_pos, test_outcome, test_pos, 
-                        conf_level=0.95, conf_type="Wilson", n_dig_prop=2, n_dig_lr=2, ...){
+                        prevalence,
+                        conf_level=0.95, conf_type="Wilson", n_dig_prop=2, n_dig_lr=2, n_dig_odds=3, ...){
   
   # check if positive class and negative class (entry not positive and not missing) are both found in the respective data columns
   if( sum( d[,ref_outcome] ==ref_pos,  na.rm=T )==0 ){ stop("function diagn_perf: positive class not found in reference outcome") }
@@ -74,21 +83,67 @@ diagn_perf <- function( d, ref_outcome, ref_pos, test_outcome, test_pos,
   #---------------------------------------------------------------------------------------------.
   # results table format ####
   na_num = NA*0
-  res <- tibble_row( ref_outcome=ref_outcome, ref_pos=ref_pos,
-                 test_outcome=test_outcome, test_pos=test_pos,
-                 conf_level=conf_level, conf_type=conf_type,
-                 Sensitivity=na_num, LCI_sens=na_num, UCI_sens=na_num, Sensitivity_str="",
-                 Specificity=na_num, LCI_spec=na_num, UCI_spec=na_num, Specificity_str="",
-                 FPR=na_num, LCI_FPR=na_num, UCI_FPR=na_num, FPR_str="",
-                 PPV=na_num, LCI_PPV=na_num, UCI_PPV=na_num, PPV_str="",
-                 NPV=na_num, LCI_NPV=na_num, UCI_NPV=na_num, NPV_str="",
-                 PLR=na_num, LCI_PLR=na_num, UCI_PLR=na_num, PLR_str="",
-                 NLR=na_num, LCI_NLR=na_num, UCI_NLR=na_num, NLR_str="",
-                 p_val_chisq=na_num, p_val_fisher=na_num, 
-                 tp=na_num, fp=na_num, fn=na_num, tn=na_num, 
-                 n_ref_pos=na_num, n_ref_neg=na_num, n_test_pos=na_num, n_test_neg=na_num, 
-                 n_ref=na_num, mis_ref=na_num, n_test=na_num, mis_test=na_num, 
-                 n_ref_and_test=na_num, n_total=na_num, mis_ref_or_test=na_num)
+  res <- tibble_row(
+    # ref_outcome = ref_outcome,
+    # ref_pos = ref_pos,
+    # test_outcome = test_outcome,
+    # test_pos = test_pos,
+    # conf_level = conf_level,
+    # conf_type = conf_type,
+    DR = na_num,
+    DR_lwr = na_num,
+    DR_upr = na_num,
+    DR_str = "",
+    FPR = na_num,
+    FPR_lwr = na_num,
+    FPR_upr = na_num,
+    FPR_str = "",
+    SPR = na_num,
+    SPR_lwr = na_num,
+    SPR_upr = na_num,
+    SPR_str = "",
+    Specificity = na_num,
+    Specificity_lwr = na_num,
+    Specificity_upr = na_num,
+    Specificity_str = "",
+    PPV_at_prevalence = na_num,
+    PPV_at_prevalence_lwr = na_num,
+    PPV_at_prevalence_upr = na_num,
+    PPV_at_prevalence_str = "",
+    NPV_at_prevalence = na_num,
+    NPV_at_prevalence_lwr = na_num,
+    NPV_at_prevalence_upr = na_num,
+    NPV_at_prevalence_str = "",
+    OAPR_at_prevalence = na_num,
+    OAPR_at_prevalence_lwr = na_num,
+    OAPR_at_prevalence_upr = na_num,
+    OAPR_at_prevalence_str = "",
+    # PLR = na_num,
+    # LCI_PLR = na_num,
+    # UCI_PLR = na_num,
+    # PLR_str = "",
+    # NLR = na_num,
+    # LCI_NLR = na_num,
+    # UCI_NLR = na_num,
+    # NLR_str = "",
+    # p_val_chisq = na_num,
+    # p_val_fisher = na_num,
+    tp = na_num,
+    fp = na_num,
+    fn = na_num,
+    tn = na_num,
+    n_ref_pos = na_num,
+    n_ref_neg = na_num,
+    n_test_pos = na_num,
+    n_test_neg = na_num,
+    n_ref = na_num,
+    mis_ref = na_num,
+    n_test = na_num,
+    mis_test = na_num,
+    n_ref_and_test = na_num,
+    n_total = na_num,
+    mis_ref_or_test = na_num
+  )
   
   #---------------------------------------------------------------------------------------------.
   # contingency table and read-out numbers ####
@@ -115,9 +170,23 @@ diagn_perf <- function( d, ref_outcome, ref_pos, test_outcome, test_pos,
   res$n_total[1] <- n_total <- ct["Sum","Sum"]
   res$mis_ref_or_test[1] = n_total - n_ref_and_test
   
+  # create a MODIFIED contintency table, which only changes the prevalence to accord to pre-specified one
+  # (the table is allowed to have non-integer counts; all upcoming statistical functions can deal with this)
+  ct_modified <- ct
+  ct_modified["Sum", "negative"]     <- ct["Sum","Sum"] * (1-prevalence)
+  ct_modified["Sum", "positive"]     <- ct["Sum","Sum"] * (prevalence)
+  ct_modified["negative","negative"] <- ct["Sum","Sum"] * (1-prevalence) * (ct["negative", "negative"] / ct["Sum", "negative"])
+  ct_modified["positive","negative"] <- ct["Sum","Sum"] * (1-prevalence) * (ct["positive", "negative"] / ct["Sum", "negative"])
+  ct_modified["negative","positive"] <- ct["Sum","Sum"] * (prevalence) * (ct["negative", "positive"] / ct["Sum", "positive"])
+  ct_modified["positive","positive"] <- ct["Sum","Sum"] * (prevalence) * (ct["positive", "positive"] / ct["Sum", "positive"])
+  tp_modified <- ct_modified["positive","positive"]
+  fp_modified <- ct_modified["positive","negative"]
+  fn_modified <- ct_modified["negative","positive"]
+  tn_modified <- ct_modified["negative","negative"]
+  
 
   #---------------------------------------------------------------------------------------------.
-  # performance measures: Sensitivity, Specificity, FPR, PPV, NPV ####
+  # performance measures: DR, Specificity, FPR, PPV, NPV ####
   
   if(conf_type %in% c("Wilson","wilson","W","w")){ 
     CI_method = "wilson" 
@@ -125,78 +194,107 @@ diagn_perf <- function( d, ref_outcome, ref_pos, test_outcome, test_pos,
     CI_method = "exact"
   }
     
-  # CIs according to Wilson: ####
-  
+  # formatting
   fmt = paste("%.",n_dig_prop,"f", sep="")
+  fmt_odds = paste("%.",n_dig_odds,"f", sep="")
   
-  # Sensitivity
+  # DR
   sens = binconf(x=tp, n=tp+fn, alpha=1-conf_level, method=CI_method)
-  res$Sensitivity[1] <- se <- sens[1,"PointEst"]
-  res$LCI_sens[1]    = sens[1,"Lower"]
-  res$UCI_sens[1]    = sens[1,"Upper"]
-  res$Sensitivity_str[1] = paste( sprintf( 100*res$Sensitivity[1], fmt=fmt ), 
-                               "% (", sprintf( 100*res$LCI_sens[1], fmt=fmt ), ", ", sprintf( 100*res$UCI_sens[1], fmt=fmt ), ")", sep="")
+  res$DR[1] <- se <- sens[1,"PointEst"]
+  res$DR_lwr[1]    = sens[1,"Lower"]
+  res$DR_upr[1]    = sens[1,"Upper"]
+  res$DR_str[1] = paste( sprintf( 100*res$DR[1], fmt=fmt ), 
+                               "% (", sprintf( 100*res$DR_lwr[1], fmt=fmt ), ", ", sprintf( 100*res$DR_upr[1], fmt=fmt ), ")", sep="")
     
   # Specificity
   spec = binconf(x=tn, n=tn+fp, alpha=1-conf_level, method=CI_method)
   res$Specificity[1] <- sp <- spec[1,"PointEst"]
-  res$LCI_spec[1]    = spec[1,"Lower"]
-  res$UCI_spec[1]    = spec[1,"Upper"]
+  res$Specificity_lwr[1]    = spec[1,"Lower"]
+  res$Specificity_upr[1]    = spec[1,"Upper"]
   res$Specificity_str[1] = paste( sprintf( 100*res$Specificity[1], fmt=fmt ), 
-                               "% (", sprintf( 100*res$LCI_spec[1], fmt=fmt ), ", ", sprintf( 100*res$UCI_spec[1], fmt=fmt ), ")", sep="")
+                               "% (", sprintf( 100*res$Specificity_lwr[1], fmt=fmt ), ", ", sprintf( 100*res$Specificity_upr[1], fmt=fmt ), ")", sep="")
 
+  # Screen positive rate (SPR)
+  SPR = binconf(x=fp+tp, n=tn+fn+fp+tp, alpha=1-conf_level, method=CI_method)
+  res$SPR[1]        = SPR[1,"PointEst"]
+  res$SPR_lwr[1]    = SPR[1,"Lower"]
+  res$SPR_upr[1]    = SPR[1,"Upper"]
+  res$SPR_str[1] = paste( sprintf( 100*res$SPR[1], fmt=fmt ), 
+                          "% (", sprintf( 100*res$SPR_lwr[1], fmt=fmt ), ", ", sprintf( 100*res$SPR_upr[1], fmt=fmt ), ")", sep="")
+  
   # False Positive Rate (FPR)
   FPR = binconf(x=fp, n=tn+fp, alpha=1-conf_level, method=CI_method)
   res$FPR[1]        = FPR[1,"PointEst"]
-  res$LCI_FPR[1]    = FPR[1,"Lower"]
-  res$UCI_FPR[1]    = FPR[1,"Upper"]
+  res$FPR_lwr[1]    = FPR[1,"Lower"]
+  res$FPR_upr[1]    = FPR[1,"Upper"]
   res$FPR_str[1] = paste( sprintf( 100*res$FPR[1], fmt=fmt ), 
-                                  "% (", sprintf( 100*res$LCI_FPR[1], fmt=fmt ), ", ", sprintf( 100*res$UCI_FPR[1], fmt=fmt ), ")", sep="")
+                                  "% (", sprintf( 100*res$FPR_lwr[1], fmt=fmt ), ", ", sprintf( 100*res$FPR_upr[1], fmt=fmt ), ")", sep="")
   
-  # PPV
-  PPV = binconf(x=tp, n=tp+fp, alpha=1-conf_level, method=CI_method)
-  res$PPV[1]        = PPV[1,"PointEst"]
-  res$LCI_PPV[1]    = PPV[1,"Lower"]
-  res$UCI_PPV[1]    = PPV[1,"Upper"]
-  res$PPV_str[1] = paste( sprintf( 100*res$PPV[1], fmt=fmt ), 
-                               "% (", sprintf( 100*res$LCI_PPV[1], fmt=fmt ), ", ", sprintf( 100*res$UCI_PPV[1], fmt=fmt ), ")", sep="")
-  
-  # NPV
-  NPV = binconf(x=tn, n=tn+fn, alpha=1-conf_level, method=CI_method)
-  res$NPV[1]        = NPV[1,"PointEst"]
-  res$LCI_NPV[1]    = NPV[1,"Lower"]
-  res$UCI_NPV[1]    = NPV[1,"Upper"]
-  res$NPV_str[1] = paste( sprintf( 100*res$NPV[1], fmt=fmt ), 
-                               "% (", sprintf( 100*res$LCI_NPV[1], fmt=fmt ), ", ", sprintf( 100*res$UCI_NPV[1], fmt=fmt ), ")", sep="")
-  
-  #---------------------------------------------------------------------------------------------.
-  # Diagnostic likelihood ratios with normal approximation
-  # formulas for CI-computation taken from R-package reportROC 
-  # e.g. https://github.com/cran/reportROC/blob/master/R/reportROC.R
-  
-  # PLR: positive likelihood ratio
-  fmt_lr = paste("%.",n_dig_lr,"f", sep="")
-  q = qnorm( p = 1 - (1 - conf_level)/2 )
-  res$PLR[1] <- PLR <- se / (1 - sp)
-  factor_CI = exp(  q * sqrt( (1-se) / tp + sp / fp ) )  # normal approximation of log( PLR )
-  res$LCI_PLR[1] = PLR / factor_CI
-  res$UCI_PLR[1] = PLR * factor_CI
-  res$PLR_str[1] = paste( sprintf( res$PLR[1], fmt=fmt_lr ), 
-                          " (", sprintf( res$LCI_PLR[1], fmt=fmt_lr ), ", ", sprintf( res$UCI_PLR[1], fmt=fmt_lr ), ")", sep="")
+  # PPV at a specified prevalence
+  PPV_at_prevalence <- binconf(x=tp_modified, n=tp_modified+fp_modified, alpha=1-conf_level, method=CI_method)
 
-  # NLR: positive likelihood ratio
-  res$NLR[1] <- NLR <- ( 1 - se ) / sp
-  factor_CI = exp(  q * sqrt( se / fn + ( 1 - sp ) / tn ) )  # normal approximation of log( NLR )
-  exp( 1.96*sqrt(se/fn+(1-sp)/tn))
-  res$LCI_NLR[1] = NLR / factor_CI
-  res$UCI_NLR[1] = NLR * factor_CI
-  res$NLR_str[1] = paste( sprintf( res$NLR[1], fmt=fmt_lr ), 
-                          " (", sprintf( res$LCI_NLR[1], fmt=fmt_lr ), ", ", sprintf( res$UCI_NLR[1], fmt=fmt_lr ), ")", sep="")
-
-  #---------------------------------------------------------------------------------------------.
-  # statistical tests for contingency table (complete case analysis) ####
-  res$p_val_chisq[1]  = chisq.test(  x=matrix( c(tp,fp,fn,tn), nrow=2, ncol=2)                          )$p.value
-  res$p_val_fisher[1] = fisher.test( x=matrix( c(tp,fp,fn,tn), nrow=2, ncol=2), alternative="two.sided" )$p.value
+  stopifnot( # check the point estimation approach implemented here against formula in validation plan. they should be equivalent
+    round(PPV_at_prevalence[1,"PointEst"], 12) ==  round(se * prevalence / (se * prevalence + (1 - sp) * (1-prevalence)), 12)
+  )
+  
+  res$PPV_at_prevalence[1]     = PPV_at_prevalence[1,"PointEst"]
+  res$PPV_at_prevalence_lwr[1] = PPV_at_prevalence[1,"Lower"]
+  res$PPV_at_prevalence_upr[1] = PPV_at_prevalence[1,"Upper"]
+  res$PPV_at_prevalence_str[1] = paste( sprintf( 100*res$PPV_at_prevalence[1], fmt=fmt ),
+                               "% (", sprintf( 100*res$PPV_at_prevalence_lwr[1], fmt=fmt ), ", ", sprintf( 100*res$PPV_at_prevalence_upr[1], fmt=fmt ), ")", sep="")
+  
+  # NPV at a specified prevalence
+  NPV_at_prevalence <- binconf(x=tn_modified, n=tn_modified+fn_modified, alpha=1-conf_level, method=CI_method)
+  
+  stopifnot( # check the point estimation approach implemented here against formula in validation plan. they should be equivalent
+    round(NPV_at_prevalence[1,"PointEst"], 12) == round(sp * (1 - prevalence) / (sp * (1 - prevalence) + (1 - se) * prevalence), 12)
+  )
+  
+  res$NPV_at_prevalence[1]     = NPV_at_prevalence[1,"PointEst"]
+  res$NPV_at_prevalence_lwr[1] = NPV_at_prevalence[1,"Lower"]
+  res$NPV_at_prevalence_upr[1] = NPV_at_prevalence[1,"Upper"]
+  res$NPV_at_prevalence_str[1] = paste( sprintf( 100*res$NPV_at_prevalence[1], fmt=fmt ),
+                                        "% (", sprintf( 100*res$NPV_at_prevalence_lwr[1], fmt=fmt ), ", ", sprintf( 100*res$NPV_at_prevalence_upr[1], fmt=fmt ), ")", sep="")
+  
+  # OAPR
+  res$OAPR_at_prevalence[1] <- res$PPV_at_prevalence[1] / (1-res$PPV_at_prevalence[1])
+    
+    stopifnot( # check the point estimation approach implemented here against formula in validation plan. they should be equivalent
+      round(res$OAPR_at_prevalence[1], 12) == round( se / ((1-sp) * (1-prevalence)) * prevalence, 12)
+    )
+  res$OAPR_at_prevalence_lwr[1] = res$PPV_at_prevalence_lwr[1] / (1-res$PPV_at_prevalence_lwr[1])
+  res$OAPR_at_prevalence_upr[1] = res$PPV_at_prevalence_upr[1] / (1-res$PPV_at_prevalence_upr[1])
+  res$OAPR_at_prevalence_str[1] = paste( sprintf( res$OAPR_at_prevalence[1], fmt=fmt_odds ),
+                                        " (", sprintf( res$OAPR_at_prevalence_lwr[1], fmt=fmt_odds ), ", ", sprintf( res$OAPR_at_prevalence_upr[1], fmt=fmt_odds ), ")", sep="")
+  
+  # #---------------------------------------------------------------------------------------------.
+  # # Diagnostic likelihood ratios with normal approximation
+  # # formulas for CI-computation taken from R-package reportROC 
+  # # e.g. https://github.com/cran/reportROC/blob/master/R/reportROC.R
+  # 
+  # # PLR: positive likelihood ratio
+  # fmt_lr = paste("%.",n_dig_lr,"f", sep="")
+  # q = qnorm( p = 1 - (1 - conf_level)/2 )
+  # res$PLR[1] <- PLR <- se / (1 - sp)
+  # factor_CI = exp(  q * sqrt( (1-se) / tp + sp / fp ) )  # normal approximation of log( PLR )
+  # res$LCI_PLR[1] = PLR / factor_CI
+  # res$UCI_PLR[1] = PLR * factor_CI
+  # res$PLR_str[1] = paste( sprintf( res$PLR[1], fmt=fmt_lr ), 
+  #                         " (", sprintf( res$LCI_PLR[1], fmt=fmt_lr ), ", ", sprintf( res$UCI_PLR[1], fmt=fmt_lr ), ")", sep="")
+  # 
+  # # NLR: positive likelihood ratio
+  # res$NLR[1] <- NLR <- ( 1 - se ) / sp
+  # factor_CI = exp(  q * sqrt( se / fn + ( 1 - sp ) / tn ) )  # normal approximation of log( NLR )
+  # exp( 1.96*sqrt(se/fn+(1-sp)/tn))
+  # res$LCI_NLR[1] = NLR / factor_CI
+  # res$UCI_NLR[1] = NLR * factor_CI
+  # res$NLR_str[1] = paste( sprintf( res$NLR[1], fmt=fmt_lr ), 
+  #                         " (", sprintf( res$LCI_NLR[1], fmt=fmt_lr ), ", ", sprintf( res$UCI_NLR[1], fmt=fmt_lr ), ")", sep="")
+  # 
+  # #---------------------------------------------------------------------------------------------.
+  # # statistical tests for contingency table (complete case analysis) ####
+  # res$p_val_chisq[1]  = chisq.test(  x=matrix( c(tp,fp,fn,tn), nrow=2, ncol=2)                          )$p.value
+  # res$p_val_fisher[1] = fisher.test( x=matrix( c(tp,fp,fn,tn), nrow=2, ncol=2), alternative="two.sided" )$p.value
 
   ## Return output table
   return(res)
